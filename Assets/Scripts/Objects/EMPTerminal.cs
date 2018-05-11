b9457aa175ad54d9311bcb80f57a12174fb6f9ca
Ashley Crouch <ashley.crouch@rocketmail.com>
2018-05-11 12:48:44 -0400

    notifies players when terminals are activated and generators are destroyed
    basic state stuff is in

--- Assets/Scripts/Objects/EMPTerminal.cs
+++ Assets/Scripts/Objects/EMPTerminal.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class EMPTerminal : Interactable {
     public bool activated = false;
+    private StateManager sm;
 
     public override void Interact(GameObject player)
     {
         if(player.tag == "Cog")
         {
             activated = true;
+            sm.SendMessage("EMPActivated");
         }
     }
 
     // Use this for initialization
     void Start () {
-		
-	}
+        sm = FindObjectOfType<StateManager>();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class StateManager : MonoBehaviour {
 
     public Generator[] generators;
+    public EMPTerminal[] EMPs;
+    public Cog[] cogs;
+    public Nutech nt;
 
 	// Use this for initialization
 	void Start () {
 
         generators = FindObjectsOfType<Generator>();
-		
+        EMPs = FindObjectsOfType<EMPTerminal>();
 	}
 	
 	// Update is called once per frame
@@ -21,5 +24,10 @@ public class StateManager : MonoBehaviour {
     public void GeneratorDestroyed()
     {
         Debug.Log("You just lost a generator!");
+
+    }
+    public void EMPActivated()
+    {
+        Debug.Log("You activated a terminal!!!!!");
     }
 }
